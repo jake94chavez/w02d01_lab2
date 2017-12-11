8d704103d1def7842ae00597ac36ac2e9413df02
Jake Chavez <jake94chavez@gmail.com>
2017-12-11 16:02:12 -0600

    Solar System Part 1 completed

--- solar_system.js
+++ solar_system.js
@@ -9,13 +9,37 @@ const solarSystem = [
 	{ name: "Neptune", ringSystem: true, moons: ["Triton", "Nereid"] }
 ];
 
+// Q1
 console.log(solarSystem[4].moons);
+// => ['Europa', 'Ganymede', 'Io', 'Callisto']
+
+// Q2
 console.log(solarSystem[7].moons[1]);
+// => Nereid
+
+// Q3
 solarSystem[1].moons.push("Endor");
+
+// Q4
 let pluto = {
 	name: "Pluto",
 	ringSystem: false,
 	moons: ["Charon"],
 }
-solarSystem.push(pluto)
-console.log(solarSystem);
+solarSystem.push(pluto);
+
+// Q5
+solarSystem[2].diameter = "7,917.5 mi";
+
+// Q6
+solarSystem[0].ringSystem = true;
+
+// Q7
+solarSystem[6].moons[3] = "Oberon";
+
+// Q8
+for (i = 0; i < solarSystem.length; i++) {
+	if (solarSystem[i].ringSystem === true) {
+		console.log(solarSystem[i]);
+	}
+}
